b072469bd792d58785b3acd73029f41caf0ea3c7
LauraGonzdc <lgonzdc@gmail.com>
2021-03-09 17:15:06 +0100

    fiche notions testing + exos

---
+++ math.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function remove (a,b){
+    return a - b;
+}
+
+module.exports.remove = remove;
+module.exports.sum = sum;
---
+++ math.test.js
@@ -0,0 +1,10 @@
+const sum = require("./math.js");
+const math = require("./math.js")
+
+it("adds 1 + 2 equals 3", ()=> {
+    expect(math.sum(1,2)).toBe(3);
+});
+
+it("removes 2 from 1 equals 1", ()=> {
+    expect(math.remove(2,1)).toBe(1);
+});
