d8ef4f928af1cc68eda31707293e7d5cb19b3828
Camille Lafrance <fripouilledepremiere@iMac-de-Camille.local>
2021-03-10 16:37:45 +0100

    fizzbuzz V1

---
+++ Pratiquer/FizzBuzz/index.js
@@ -0,0 +1,12 @@
+function fizzbuzz(number){
+    let toFizzbuzz = "";
+    return toFizzbuzz;
+}
+
+function mainLoop(){
+
+
+}
+
+
+module.exports = fizzbuzz;
---
+++ Pratiquer/FizzBuzz/index.test.js
@@ -0,0 +1,53 @@
+const fizzbuzz = require("./index.js"); 
+
+test('3 returns fizz', () => {
+    expect(fizzbuzz(3)).toBe("fizz");
+});
+
+test('5 returns buzz', () => {
+    expect(fizzbuzz(5)).toBe("buzz");
+});
+
+test('15 returns fizzbuzz', () => {
+    expect(fizzbuzz(15)).toBe("fizzbuzz");
+});
+
+test('1 returns 1', () => {
+    expect(fizzbuzz(1)).toBe("1");
+});
+
+test('toto returns error', () => {
+    expect(fizzbuzz("toto")).toBe("error");
+});
+
+test('empty string returns error', () => {
+    expect(fizzbuzz("")).toBe("error");
+});
+
+test('-10 returns error', () => {
+    expect(fizzbuzz(-10)).toBe("error");
+});
+
+test('1.1 returns error', () => {
+    expect(fizzbuzz(1.1)).toBe("error");
+});
+
+test('0 returns fizzbuzz', () => {
+    expect(fizzbuzz(0)).toBe("fizzbuzz");
+});
+
+test('53 returns 53', () => {
+    expect(fizzbuzz(53)).toBe("53");
+});
+
+test('105 returns fizzbuzz', () => {
+    expect(fizzbuzz(105)).toBe("fizzbuzz");
+});
+
+test('90 returns fizzbuzz', () => {
+    expect(fizzbuzz(90)).toBe("fizzbuzz");
+});
+
+// test('adds 1 + 2 to equal 3', () => {
+//    expect(sum(1, 2)).toBe(3);
+//  });
