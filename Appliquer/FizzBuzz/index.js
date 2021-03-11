ae1131f17cbddde715feb75dc5ef31573e30d7b5
Camille Lafrance <fripouilledepremiere@iMac-de-Camille.local>
2021-03-11 15:30:02 +0100

    appliquer version finale

--- Appliquer/FizzBuzz/index.js
+++ Appliquer/FizzBuzz/index.js
@@ -1,34 +1,32 @@
-function fizzbuzz(number){
-        if (!(typeof number === "number")){
-            return undefined
-        }
-        if (!(number >= 0 && number < 101)||(!(Number.isInteger(number)))){
-            return "error"
-        }
-        if (number %3 == 0 && number %5 == 0){
-            return "fizzbuzz"
-        }
-        else if (number %3 == 0){
-            return "fizz"
-        }
-        else if (number %5 == 0){
-         return "buzz"
-        }
-        else {
-             return number.toString()
-         }
-    
+function fizzbuzz(number) {
+    if (!(typeof number === "number")) {
+        return undefined;
+    }
+    else if (!(number >= 0 && number < 101)||(!(Number.isInteger(number)))) {
+        return "error";
+    }
+    else if (number %3 == 0 && number %5 == 0) {
+        return "fizzbuzz";
+    }
+    else if (number %3 == 0) {
+        return "fizz";
+    }
+    else if (number %5 == 0) {
+        return "buzz";
+    }
+    else {
+        return number.toString();
+    }
  }
- 
- let number=0;
 
- function showFizzbuzz(){
-     while (number>=0 && number<101){
-         console.log(fizzbuzz(number));
-         number++;
-     }
- }
-     
+function showFizzbuzz() {
+    let number = 0;
+    while (number >= 0 && number < 101) {
+        console.log(fizzbuzz(number));
+        number++;
+    }
+ } 
+
  showFizzbuzz();
 
  module.exports = fizzbuzz;
